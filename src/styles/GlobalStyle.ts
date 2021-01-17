38bfd3aebc013a89c0b2827dd97e2f603cb0c3f8
maxleon52 <maxleon522@gmail.com>
2021-01-17 20:55:46 -0300

    first commit

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+    *{
+      margin: 0;
+      padding: 0;
+    }
+
+    body{
+      background: #F5F5F5;
+      font: 400 16px 'Source Sans Pro', sans-serif;
+    }
+`
